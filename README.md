a87fb5dd42025649b1a209e4aee3b28647737beb
D4D3v4l <diegocollantes202010@gmail.com>
2026-05-12 14:15:57 -0500

    feat: initial commit

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
-# gigmap-landingpage
+# GigMap-LandingPage-V2
+GigMap-LandingPage-V2
---
+++ astro.config.mjs
@@ -0,0 +1,19 @@
+// @ts-check
+import { defineConfig } from "astro/config";
+import tailwindcss from "@tailwindcss/vite";
+
+// https://astro.build/config
+export default defineConfig({
+  site: "https://StayBitsV2.github.io/",
+  base: "gigmap-landingpage",
+  i18n: {
+    defaultLocale: "en",
+    locales: ["es", "en"],
+    routing: {
+      prefixDefaultLocale: false,
+    },
+  },
+  vite: {
+    plugins: [tailwindcss()],
+  },
+});
